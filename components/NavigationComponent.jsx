f2fa9ba28a34f793a08dba53eaca4b9705fdb31a
Juan Andres Vega <juanandres.vega@dualbootpartners.com>
2022-12-06 18:43:43 -0300

    First commit - Add navigation and page Layout

---
+++ components/NavigationComponent.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from '../styles/Navigation.module.css';
+
+export default function NavigationComponent({ links = [] }){
+    return (
+        <header className={styles.header}>
+            <ul className={styles.ul}>
+                {
+                    links.map((link, index) => (
+                        <li key={`${index}_navigation`} className={styles.li}>
+                            <Link href={link.path}>{link.name}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </header>
+    )
+}
